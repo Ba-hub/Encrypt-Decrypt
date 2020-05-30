1fe22dbddd00bdbf1074936e01d0754f9697681f
Ba-hub <54659606+Ba-hub@users.noreply.github.com>
2020-05-30 12:48:01 +0530

    Update installer.py

--- installer.py
+++ installer.py
@@ -12,7 +12,7 @@ class color:
 
 opt = raw_input("\n [?]" + color.INFO + "Install necessary libraries (y/n): " + color.ENDC)
 if opt.lower() == "y":
-    print("\n [+]" + color.INFO + " Install libreries... \n" + color.ENDC)
+    print("\n [+]" + color.INFO + " Installing libreries please wait... \n" + color.ENDC)
     os.system('sudo pip install urllib3 passlib argparse')
     os.system('sudo easy_install hashlib')
     print("\n [+]" + color.GREEN + " Libraries installed!!\n" + color.ENDC)
